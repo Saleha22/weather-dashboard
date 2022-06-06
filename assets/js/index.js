389f0b7eac8dea375f62dc9ea8549b02baa6c50e
Saleha22 <102179415+Saleha22@users.noreply.github.com>
2022-06-06 19:42:57 +0100

    working on display render forecast

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
 const API_KEY = "b8753c1d706e3e5284cd9616c6e90c03";
+//DOM SELECTOR
+var searchForm = document.getElementById("search-form");
+
+var forecastContainer = document.querySelector("#forecast");
 
 const renderCities = () => {
   // get recent cities from LS []
@@ -18,10 +22,76 @@ const renderCurrentWeather = (currentWeatherData) => {
   // render the current weather data and append to section
 };
 
-const renderForecastWeather = (forecastWeatherData) => {
-  // render the forecast weather data and append each card to section
-};
+// Function to display a forecast card given an object from open weather api
+// daily forecast.
+function renderForecastCard(forecast, timezone) {
+  // variables for data from api
+  var unixTs = forecast.dt;
+  var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+  var iconDescription = forecast.weather[0].description;
+  var tempF = forecast.temp.day;
+  var { humidity } = forecast;
+  var windMph = forecast.wind_speed;
 
+  // Create elements for a card
+  var col = document.createElement("div");
+  var card = document.createElement("div");
+  var cardBody = document.createElement("div");
+  var cardTitle = document.createElement("h5");
+  var weatherIcon = document.createElement("img");
+  var tempEl = document.createElement("p");
+  var windEl = document.createElement("p");
+  var humidityEl = document.createElement("p");
+
+  col.append(card);
+  card.append(cardBody);
+  cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+
+  col.setAttribute("class", "col-md");
+  col.classList.add("five-day-card");
+  card.setAttribute("class", "card bg-primary h-100 text-white");
+  cardBody.setAttribute("class", "card-body p-2");
+  cardTitle.setAttribute("class", "card-title");
+  tempEl.setAttribute("class", "card-text");
+  windEl.setAttribute("class", "card-text");
+  humidityEl.setAttribute("class", "card-text");
+
+  // Add content to elements
+  cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format("M/D/YYYY");
+  weatherIcon.setAttribute("src", iconUrl);
+  weatherIcon.setAttribute("alt", iconDescription);
+  tempEl.textContent = `Temp: ${tempF} °F`;
+  windEl.textContent = `Wind: ${windMph} MPH`;
+  humidityEl.textContent = `Humidity: ${humidity} %`;
+
+  forecastContainer.append(col);
+}
+
+// Function to display 5 day forecast.
+function renderForecast(dailyForecast, timezone) {
+  // Create unix timestamps for start and end of 5 day forecast
+  var startDt = dayjs().tz(timezone).add(1, "day").startOf("day").unix();
+  var endDt = dayjs().tz(timezone).add(6, "day").startOf("day").unix();
+
+  var headingCol = document.createElement("div");
+  var heading = document.createElement("h4");
+
+  headingCol.setAttribute("class", "col-12");
+  heading.textContent = "5-Day Forecast:";
+  headingCol.append(heading);
+
+  forecastContainer.innerHTML = "";
+  forecastContainer.append(headingCol);
+  for (var i = 0; i < dailyForecast.length; i++) {
+    // The api returns forecast data which may include 12pm on the same day and
+    // always includes the next 7 days. The api documentation does not provide
+    // information on the behavior for including the same day. Results may have
+    // 7 or 8 items.
+    if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
+      renderForecastCard(dailyForecast[i], timezone);
+    }
+  }
+}
 const renderWeatherData = (cityName) => {
   // use API to fetch current weather data
 
@@ -79,7 +149,7 @@ const onReady = () => {
   renderCities();
 };
 
-const searchCity = () => {
+const handleSearchFormSubmit = () => {
   var city = document.getElementById("City").value;
   renderWeatherData(city);
 };
@@ -103,6 +173,7 @@ const getWeatherforcast = (lon, lat) => {
         result += `Humidity: ${data["main"]["humidity"]}%<br>`;
         result += `Wind: ${data["wind"]["speed"]}MPH<br></div>`;
         $("#FiveDayForecast").html(result);
+        renderForecast(data["daily"], data["timezone"]);
       }
     })
     .catch(function (err) {
@@ -111,3 +182,4 @@ const getWeatherforcast = (lon, lat) => {
 };
 
 $(document).ready(onReady);
+searchForm.addEventListener("click", handleSearchFormSubmit);
