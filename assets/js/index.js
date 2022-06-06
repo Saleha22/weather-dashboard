11e2371570ad2b980eb52b195461b8c16a57d50f
Saleha22 <102179415+Saleha22@users.noreply.github.com>
2022-06-06 19:56:27 +0100

    included important dayjs plug in

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,9 @@ const API_KEY = "b8753c1d706e3e5284cd9616c6e90c03";
 var searchForm = document.getElementById("search-form");
 
 var forecastContainer = document.querySelector("#forecast");
-
+// Add timezone plugins to day.js
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
 const renderCities = () => {
   // get recent cities from LS []
   var searchResults = localStorage.getItem("searchResults");
@@ -69,10 +71,11 @@ function renderForecastCard(forecast, timezone) {
 
 // Function to display 5 day forecast.
 function renderForecast(dailyForecast, timezone) {
+  alert(timezone);
   // Create unix timestamps for start and end of 5 day forecast
   var startDt = dayjs().tz(timezone).add(1, "day").startOf("day").unix();
   var endDt = dayjs().tz(timezone).add(6, "day").startOf("day").unix();
-
+  console.log(timezone);
   var headingCol = document.createElement("div");
   var heading = document.createElement("h4");
 
@@ -114,7 +117,6 @@ const renderWeatherData = (cityName) => {
         renderCities();
 
         // appendToHistory(search);
-        getWeatherforcast(data["coord"]["lon"], data["coord"]["lat"]);
         var icon = data["weather"][0]["icon"];
         var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
@@ -123,6 +125,8 @@ const renderWeatherData = (cityName) => {
         result += `Humidity: ${data["main"]["humidity"]}%<br>`;
         result += `Wind: ${data["wind"]["speed"]}MPH<br>`;
         $("#result").html(result);
+        console.log("renderForecast");
+        renderForecast(data["daily"], data["timezone"]);
       }
     })
     .catch(function (err) {
@@ -152,33 +156,6 @@ const onReady = () => {
 const handleSearchFormSubmit = () => {
   var city = document.getElementById("City").value;
   renderWeatherData(city);
-};
-
-const getWeatherforcast = (lon, lat) => {
-  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&cnt=5&appid=${API_KEY}`;
-  fetch(currentWeatherUrl)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      console.log(data);
-
-      if (data["main"]) {
-        // appendToHistory(search);
-        var icon = data["weather"][0]["icon"];
-        var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-        var result = `<div class='weatherBox'><h2>${cityName}</h2>${date}<br/><img src="http://openweathermap.org/img/wn/${icon}@2x.png">`;
-        result += `Temp: ${data["main"]["temp"]}F<br>`;
-        result += `Humidity: ${data["main"]["humidity"]}%<br>`;
-        result += `Wind: ${data["wind"]["speed"]}MPH<br></div>`;
-        $("#FiveDayForecast").html(result);
-        renderForecast(data["daily"], data["timezone"]);
-      }
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
 };
 
 $(document).ready(onReady);
