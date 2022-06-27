75e48f01b14faf311cdb82905d69e3f8cce3a139
Saleha22 <102179415+Saleha22@users.noreply.github.com>
2022-06-27 21:30:20 +0100

    added changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,8 +20,46 @@ const renderCities = () => {
   // add an event listener on div containing all cities
 };
 
-const renderCurrentWeather = (currentWeatherData) => {
-  // render the current weather data and append to section
+const formatTime = (currentWeatherTime) => {
+  const convertedDate = new Date(currentWeatherTime * 1000);
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const year = convertedDate.getFullYear();
+  const month = months[convertedDate.getMonth()];
+  const date = convertedDate.getDate();
+  // return the time in as date, month, year format
+  const time = date + " " + month + " " + year;
+  return time;
+};
+const renderCurrentWeatherCard = (currentWeatherData) => {
+  console.log(currentWeatherData);
+  const date = formatTime(currentWeatherData.dt);
+  const iconUrl = `https://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`;
+  const card = `<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=${iconUrl} alt="weatherForecastCardDay1">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text"> date: ${date} </p>
+    <p class="card-text"> temp: ${currentWeatherData.temp.day} degrees</p>
+    <p class="card-text"> wind speed: ${currentWeatherData.wind_speed} mph</p>
+    <p class="card-text"> humidity: ${currentWeatherData.humidity} % </p>
+    <p class="card-text"> uv-index: ${currentWeatherData.uvi} </p>
+  </div>
+</div>`;
+
+  $("#result").append(card);
 };
 
 // Function to display a forecast card given an object from open weather api
@@ -95,47 +133,52 @@ function renderForecast(dailyForecast, timezone) {
     }
   }
 }
-const renderWeatherData = (cityName) => {
+const renderWeatherData = async (cityName) => {
   // use API to fetch current weather data
 
   const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
 
-  fetch(currentWeatherUrl)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      console.log(data);
+  try {
+    const currentWeatherData = await fetch(currentWeatherUrl);
+    const currentData = await currentWeatherData.json();
 
-      if (!data["main"]) {
-        alert("Location not found");
-      } else {
-        var searchResults = localStorage.getItem("searchResults");
-        searchResults = searchResults + "|" + cityName;
-        window.localStorage.setItem("searchResults", searchResults);
-        console.log(searchResults);
-        renderCities();
+    if (!currentData["main"]) {
+      alert("Location not found");
+    } else {
+      const lat = currentData.coord.lat;
+      const lon = currentData.coord.lon;
+      const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}`;
+      const forecastWeatherData = await fetch(forecastWeatherUrl);
+      const forecastData = await forecastWeatherData.json();
 
-        // appendToHistory(search);
-        var icon = data["weather"][0]["icon"];
-        var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+      const todaysWeather = forecastData.daily[0];
+      const forecastWeather = forecastData.daily.slice(1, 6);
 
-        var result = `<h2>${cityName}</h2>${date}<br/><img src="http://openweathermap.org/img/wn/${icon}@2x.png">`;
-        result += `Temp: ${data["main"]["temp"]}F<br>`;
-        result += `Humidity: ${data["main"]["humidity"]}%<br>`;
-        result += `Wind: ${data["wind"]["speed"]}MPH<br>`;
-        $("#result").html(result);
-        console.log("renderForecast");
-        renderForecast(data["daily"], data["timezone"]);
-      }
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
+      renderCurrentWeatherCard(todaysWeather, "Europe/London");
+
+      var searchResults = localStorage.getItem("searchResults");
+      searchResults = searchResults + "|" + cityName;
+      window.localStorage.setItem("searchResults", searchResults);
+      renderCities();
+
+      // // appendToHistory(search);
+      // var icon = data["weather"][0]["icon"];
+      // var date = moment().format("MMMM Do YYYY, h:mm:ss a");
+
+      // var result = `<h2>${cityName}</h2>${date}<br/><img src="http://openweathermap.org/img/wn/${icon}@2x.png">`;
+      // result += `Temp: ${data["main"]["temp"]}F<br>`;
+      // result += `Humidity: ${data["main"]["humidity"]}%<br>`;
+      // result += `Wind: ${data["wind"]["speed"]}MPH<br>`;
+      // $("#result").html(result);
+      // console.log("renderForecast");
+      // renderForecast(data["daily"], data["timezone"]);
+    }
+  } catch (error) {
+    console.error(error);
+  }
   // from the response cherry pick all the data you want to see in the current weather card
 
   // get the lat and lon from current weather data API response
-  //const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}`;
 
   // render current weather data
 
@@ -153,9 +196,9 @@ const onReady = () => {
   renderCities();
 };
 
-const handleSearchFormSubmit = () => {
+const handleSearchFormSubmit = async () => {
   var city = document.getElementById("City").value;
-  renderWeatherData(city);
+  await renderWeatherData(city);
 };
 
 $(document).ready(onReady);
